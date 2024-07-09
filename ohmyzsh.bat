82e6e407dd87889ac22b0759c427e9b55bb82f22
Jianshan Jiang <jiangjianshan1103@gmail.com>
2024-07-09 02:13:37 +0100

    Fix issue of file path can not be found

--- ohmyzsh.bat
+++ ohmyzsh.bat
@@ -3,50 +3,51 @@
 setlocal enabledelayedexpansion
 
 :: ohmyzsh
-if not exist "%HOME:\=/%/.oh-my-zsh" (
+if not exist "!HOME!\.oh-my-zsh" (
   echo [%~nx0] Installing ohmyzsh
-  git clone --depth 1 --single-branch -b master https://github.com/ohmyzsh/ohmyzsh.git %HOME:\=/%/.oh-my-zsh || exit /b 1
-  copy /V /Y %HOME%\.oh-my-zsh\templates\zshrc.zsh-template %HOME%\.zshrc
+  git clone --single-branch -b master https://github.com/ohmyzsh/ohmyzsh.git !HOME!\.oh-my-zsh || goto :end
+  copy /V /Y !HOME!\.oh-my-zsh\templates\zshrc.zsh-template !HOME!\.zshrc
+  C:\cygwin64\bin\sed -i "s|ZSH_THEME=\"robbyrussell\"|ZSH_THEME=\"powerlevel10k\/powerlevel10k\"|g" !HOME:\=/!/.zshrc
+  C:\cygwin64\bin\sed -i "s|plugins=(git)|plugins=(git zsh-autosuggestions zsh-syntax-highlighting)|g" !HOME:\=/!/.zshrc
 ) else (
   echo [%~nx0] Updating ohmyzsh
-  cd /d %HOME:\=/%/.oh-my-zsh
-  git fetch origin master --depth 1 || exit /b 1
-  git reset --hard origin/master || exit /b 1
+  cd /d !HOME!\.oh-my-zsh
+  git fetch origin master || goto :end
+  git reset --hard origin/master || goto :end
 )
 
 :: powerlevel10k
-if not exist "%HOME:\=/%/.oh-my-zsh/custom/themes/powerlevel10k" (
+if not exist "!HOME!\.oh-my-zsh\custom\themes\powerlevel10k" (
   echo [%~nx0] Installing powerlevel10k
-  git clone --depth 1 --single-branch -b master https://github.com/romkatv/powerlevel10k.git %HOME:\=/%/.oh-my-zsh/custom/themes/powerlevel10k || exit /b 1
-  sed -i 's|ZSH_THEME=\"robbyrussell\"|ZSH_THEME=\"powerlevel10k\/powerlevel10k\"|g' %HOME:\=/%/.zshrc
-  sed -i 's|plugins=(git)|plugins=(git zsh-autosuggestions zsh-syntax-highlighting)|g' %HOME:\=/%/.zshrc
+  git clone --single-branch -b master https://github.com/romkatv/powerlevel10k.git !HOME!\.oh-my-zsh\custom\themes\powerlevel10k || goto :end
 ) else (
   echo [%~nx0] Updating powerlevel10k
-  cd /d %HOME:\=/%/.oh-my-zsh/custom/themes/powerlevel10k
-  git fetch origin master --depth 1 || exit /b 1
-  git reset --hard origin/master || exit /b 1
+  cd /d !HOME:\=/!\.oh-my-zsh/custom/themes/powerlevel10k
+  git fetch origin master || goto :end
+  git reset --hard origin/master || goto :end
 )
 
 :: zsh-autosuggestions
-if not exist "%HOME:\=/%/.oh-my-zsh/custom/plugins/zsh-autosuggestions" (
+if not exist "!HOME!\.oh-my-zsh\custom\plugins\zsh-autosuggestions" (
   echo [%~nx0] Installing zsh-autosuggestions
-  git clone --depth 1 --single-branch -b master https://github.com/zsh-users/zsh-autosuggestions %HOME:\=/%/.oh-my-zsh/custom/plugins/zsh-autosuggestions || exit /b 1
+  git clone --single-branch -b master https://github.com/zsh-users/zsh-autosuggestions !HOME!\.oh-my-zsh\custom\plugins\zsh-autosuggestions || goto :end
 ) else (
   echo [%~nx0] Updating zsh-autosuggestions
-  cd %HOME:\=/%/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-  git fetch origin master --depth 1 || exit /b 1
-  git reset --hard origin/master || exit /b 1
+  cd !HOME:\=/!\.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git fetch origin master || goto :end
+  git reset --hard origin/master || goto :end
 )
 
 :: zsh-syntax-highlighting
-if not exist "%HOME:\=/%/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" (
+if not exist "!HOME!\.oh-my-zsh\custom\plugins\zsh-syntax-highlighting" (
   echo [%~nx0] Installing zsh-syntax-highlighting
-  git clone --depth 1 --single-branch -b master https://github.com/zsh-users/zsh-syntax-highlighting.git %HOME:\=/%/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting || exit /b 1
+  git clone --single-branch -b master https://github.com/zsh-users/zsh-syntax-highlighting.git !HOME!\.oh-my-zsh\custom\plugins\zsh-syntax-highlighting || goto :end
 ) else (
   echo [%~nx0] Updating zsh-syntax-highlighting
-  cd %HOME:\=/%/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-  git fetch origin master --depth 1 || exit /b 1
-  git reset --hard origin/master || exit /b 1
+  cd !HOME!\.oh-my-zsh\custom\plugins\zsh-syntax-highlighting
+  git fetch origin master || goto :end
+  git reset --hard origin/master || goto :end
 )
 
+:end
 echo [%~nx0] Done.
